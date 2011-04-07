bd9b5628214f3b4e1affc911538d30bb42442cce
Yuval Adam <yuv.adm@gmail.com>
2011-04-07 23:04:16 +0300

    solved a

--- 2010/qual/a/a.py
+++ 2010/qual/a/a.py
@@ -1,5 +1,5 @@
-f = open('1.in', 'r')
-o = open('1.out', 'w')
+f = open('3.in', 'r')
+o = open('3.out', 'w')
 
 t = int(f.readline().strip())
 
@@ -8,25 +8,8 @@ for i in range(t):
     n = int(nk[0])
     k = int(nk[1])
     
-    a = [(0, 0)]*n
-    a[0] = (1, 0)
-
-    for j in range(k):
-        for y in range(n):
-            if a[y][0]==1:
-                a[y] = (a[y][0], a[y][1]^1)
-        
-        for y in range(n):
-            if y==0:
-                a[y] = (1, a[y][1])
-            else:
-                a[y] = (a[y-1][0]*a[y-1][1], a[y][1])
-
-#    print a[n-1][0], a[n-1][1]
+    res = 'ON' if k & 2**n-1 == 2**n-1 else 'OFF'
     
-    s = "Case #" + str(i+1) + ": OFF\n"
-    if a[n-1][0]*a[n-1][1]==1:
-        s = "Case #" + str(i+1) + ": ON\n"
+    s = "Case #%d: %s\n" % (i+1, res)
+
     o.write(s)
-    print s
-    
