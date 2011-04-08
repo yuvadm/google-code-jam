2aeb590a846982a09e96c15756175a616b8647c7
Yuval Adam <yuv.adm@gmail.com>
2011-04-08 11:31:25 +0300

    added template

---
+++ template.py
@@ -0,0 +1,11 @@
+f = open('1.in', 'r')
+o = open('1.out', 'w')
+
+t = int(f.readline().strip())
+
+for i in range(t):
+    nk = f.readline().strip().split(' ')
+    n = int(nk[0])
+    k = int(nk[1])
+    
+    o.write(s)
