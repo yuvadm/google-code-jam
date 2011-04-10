8e1d5a67f66c5de7983aab6ed681f52ecf1c2242
Yuval Adam <yuv.adm@gmail.com>
2011-04-10 23:31:41 +0300

    solved fair warning

---
+++ 2010/qual/b/b.py
@@ -0,0 +1,21 @@
+f = open('2.in', 'r')
+o = open('2.out', 'w')
+
+c = int(f.readline().strip())
+
+def gcd(a, b):
+    return a if b==0 else gcd(b, a % b)
+
+for i in range(c):
+    t = map(int, f.readline().strip().split(' ')[1:])
+    l = [abs(x - t[0]) for x in t]
+    g = reduce(gcd, l)
+    
+    if t[0] % g == 0:
+        r = 0
+    else:
+        r = g - (t[0] % g)
+    
+    s = "Case #%d: %s\n" % (i+1, r)
+
+    o.write(s)
