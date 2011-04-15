afaadd56790f0d573f5cd716dc0af23836abe343
Yuval Adam <yuv.adm@gmail.com>
2011-04-15 12:54:53 +0300

    solved b, easy peasy

--- 2010/af_qual/b/b.py
+++ 2010/af_qual/b/b.py
@@ -5,19 +5,8 @@ o = open('3.out', 'w')
 
 n = int(f.readline().strip())
 
-for j in range(n):
-    c = int(f.readline().strip())
-    i = int(f.readline().strip())
-    P = map(int, f.readline().strip().split(' '))
-    
-    res = (0, 0)
-    for k in range(len(P)):
-        for l in range(k+1, len(P)):
-            if P[k]+P[l]==c:
-                res = [k+1, l+1]
-                res = ' '.join(map(str, res))
-                break
-    
-    s = "Case #%d: %s\n" % (j+1, res)
-    
+for i in range(n):
+    l = f.readline().strip()
+    res = ' '.join(l.split(' ')[::-1])
+    s = "Case #%d: %s\n" % (i+1, res)
     o.write(s)
