354967bfde9a3c5359dfc62287d2748ad89ae531
Yuval Adam <yuv.adm@gmail.com>
2011-04-16 00:43:00 +0300

    got the rotate function down

--- 2010/1a/a.py
+++ 2010/1a/a.py
@@ -1,11 +1,20 @@
 f = open('1.in', 'r')
 o = open('1.out', 'w')
 
+# rotate *90 degrees clockwise*
+def rotate(m):
+    n = len(m)
+    return [''.join([m[j][i] for j in range(n)[::-1]]) for i in range(n)]
+
 t = int(f.readline().strip())
 
 for i in range(t):
-    nk = f.readline().strip().split(' ')
+    (n, k) = map(int, f.readline().strip().split(' '))
     
-    s = "Case #%d: %s\n" % (i+1, res)
-
+    mat = [f.readline().strip() for j in range(n)]
+    
+    res = rotate(mat)
+    
+    s = "Case #%d: \n%s\n" % (i+1, '\n'.join(res))
+    print s
     o.write(s)
