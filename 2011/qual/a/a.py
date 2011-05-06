4e133440c26caff5a46b684eaec25794642db299
Yuval Adam <yuv.adm@gmail.com>
2011-05-06 11:47:30 +0300

    preparing a for qual 2011

---
+++ 2011/qual/a/a.py
@@ -0,0 +1,11 @@
+f = open('1.in', 'r')
+o = open('1.out', 'w')
+
+T = int(f.readline().strip())
+
+for t in xrange(T):
+    (n, k) = map(int, f.readline().strip().split(' '))
+    
+    s = "Case #%d: %s\n" % (t+1, res)
+    #print s
+    o.write(s)
