50df8b05566709fe306efa360e57976ac5720121
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 16:32:52 +0300

    updated d

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -7,15 +7,15 @@ for t in xrange(T):
     _n = f.readline().strip()
     el = map(int, f.readline().strip().split(' '))
     print el
-    mixed = len([el[i] for i in range(len(el)) if el[i] != i+1])
-    pairs = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] == i+1])
-    mpd = mixed - pairs
-    if mpd > 0:
-        mpd -= 1
-    steps = pairs + (2 * mpd)
+    m = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] != i+1])
+    p = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] == i+1])
+
+    if m > 0:
+        m -= 1
+    steps = p + (2 * m)
     
     res = steps
-    print mixed, pairs
+    print m, p
     s = "Case #%d: %s\n" % (t+1, res)
     print s
     o.write(s)
