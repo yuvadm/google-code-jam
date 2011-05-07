264b05c568532e966d5eb310b750df22065a38a7
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 22:31:40 +0300

    omfg, it works

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -1,5 +1,5 @@
-f = open('2.in', 'r')
-o = open('2.out', 'w')
+f = open('3.in', 'r')
+o = open('3.out', 'w')
 
 T = int(f.readline().strip())
 
@@ -10,7 +10,7 @@ for t in xrange(T):
     res = len([el[i] for i in range(len(el)) if el[i] != i+1])
     
     s = "Case #%d: %s\n" % (t+1, res)
-    print s
+    #print s
     o.write(s)
 
 f.close()
