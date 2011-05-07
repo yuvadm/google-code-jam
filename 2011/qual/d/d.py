fe8ca301ff2bba036ead7273168357879adc1dfd
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 17:32:30 +0300

    i think i got it

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -6,31 +6,28 @@ def solve(a):
     # stop if solved
     while a != sol:
         print "%s is not %s" % (a, sol)
-        
+        flipped = False
         # first flip all pairs
         for i in range(len(a)):
             if a[i] != i+1 and a[a[i]-1] == i+1:
                 print 'flipping %d and %d' % (a[a[i]-1], a[i])
-                t = a[i]
-                a[i] = a[a[i]-1]
-                print 'flipping %d and %d' % (a[a[i]-1], a[i])
-                a[a[i]-1] = t
-                print 'flipping %d and %d' % (a[a[i]-1], a[i])
-                break
+                t = a[a[i]-1]
+                a[a[i]-1] = a[i]
+                a[i] = t
                 steps += 2
-        
-        # did that suffice?
-        if a == sol:
-            print "%s is not %s" % (a, sol)
-            break
+                flipped = True
+    
+        if flipped:
+            continue
         
         # now find the first mixed index to fix
         for i in range(len(a)):
             if a[i] != i+1:
                 print 'flipping %d and %d' % (a[a[i]-1], a[i])
-                a[i], a[a[i]-1] = a[a[i]-1], a[i]
+                t = a[a[i]-1]
+                a[a[i]-1] = a[i]
+                a[i] = t
                 steps += 2
-        sleep(1)
     return steps
         
 
