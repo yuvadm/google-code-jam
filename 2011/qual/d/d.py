09869227fe2beb5b7dbb3dc01293c34d57f5fd67
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 16:40:42 +0300

    moved m and p print

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -9,13 +9,15 @@ for t in xrange(T):
     print el
     m = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] != i+1])
     p = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] == i+1])
-
+    
+    print m, p
+    
     if m > 0:
         m -= 1
     steps = p + (2 * m)
     
     res = steps
-    print m, p
+    
     s = "Case #%d: %s\n" % (t+1, res)
     print s
     o.write(s)
