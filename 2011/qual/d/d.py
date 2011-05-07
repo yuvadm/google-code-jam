6f582eb9f9440334c4a6ff6678b72db011d165d7
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 16:31:52 +0300

    working on d small

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -6,14 +6,18 @@ T = int(f.readline().strip())
 for t in xrange(T):
     _n = f.readline().strip()
     el = map(int, f.readline().strip().split(' '))
-    
+    print el
     mixed = len([el[i] for i in range(len(el)) if el[i] != i+1])
     pairs = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] == i+1])
-    steps = pairs + (2 * (mixed - pairs))
+    mpd = mixed - pairs
+    if mpd > 0:
+        mpd -= 1
+    steps = pairs + (2 * mpd)
     
     res = steps
     print mixed, pairs
     s = "Case #%d: %s\n" % (t+1, res)
+    print s
     o.write(s)
 
 f.close()
