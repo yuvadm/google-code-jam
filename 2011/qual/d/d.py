d54f52c74f4ab1b98046dc9fa1b1a8ae21c3b465
Yuval Adam <yuv.adm@gmail.com>
2011-05-07 22:29:35 +0300

    before stupid trial

--- 2011/qual/d/d.py
+++ 2011/qual/d/d.py
@@ -1,5 +1,3 @@
-
-
 f = open('2.in', 'r')
 o = open('2.out', 'w')
 
@@ -8,13 +6,8 @@ T = int(f.readline().strip())
 for t in xrange(T):
     _n = f.readline().strip()
     el = map(int, f.readline().strip().split(' '))
-
-    m = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] != i+1])
-    p = len([el[i] for i in range(len(el)) if el[i] != i+1 and el[el[i]-1] == i+1])
     
-    print m, p
-    m = m-1 if m > 0 else 0
-    res = p + (m * 2)
+    res = len([el[i] for i in range(len(el)) if el[i] != i+1])
     
     s = "Case #%d: %s\n" % (t+1, res)
     print s
