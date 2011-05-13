1ea1751b5349ede8f3c7a5f633ddaf178e472e79
Yuval Adam <yuv.adm@gmail.com>
2011-05-13 13:07:40 +0300

    simple delta algorithm isnt enough

--- 2010/1c/a.py
+++ 2010/1c/a.py
@@ -4,10 +4,20 @@ o = open('1.out', 'w')
 T = int(f.readline().strip())
 
 for t in xrange(T):
-    (n, m, o) = map(int, f.readline().strip().split(' '))
-    l = f.readline().strip()
+    N = int(f.readline().strip())
+    wires = []
+    for n in range(N):
+        wires.append(map(int, f.readline().strip().split(' ')))
     
-    res = str(n) + str(m) + str(o) + l
+    wires.sort(key=lambda wire: wire[0])
+    
+    cross = 0
+    bwires = [x[1] for x in wires]
+    print bwires
+    bwd = [bwires[i+1]-bwires[i] for i in range(len(bwires)-1)]
+    print bwd
+    res = len([x for x in bwd if x < 0])
+    
     s = "Case #%d: %s\n" % (t+1, res)
     print s
     #o.write(s)
