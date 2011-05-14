8265fe327cc95f32c345b663d9b31ef377e5ec62
Yuval Adam <yuv.adm@gmail.com>
2011-05-14 22:35:26 +0300

    got the bit mask

--- 2010/1c/c/c.py
+++ 2010/1c/c/c.py
@@ -1,11 +1,17 @@
 f = open('1.in', 'r')
 o = open('1.out', 'w')
 
+M1 = 0xaaaaaaaa
+M0 = M1 >> 1
+
 T = int(f.readline().strip())
 
 for t in xrange(T):
     (M, N) = map(int, f.readline().strip().split(' '))
     grid = [int(f.readline().strip(), 16) for _m in range(M)]
+    
+    nextSquare = min(M, N)
+    
     
     res = grid
     s = "Case #%d: %s\n" % (t+1, res)
