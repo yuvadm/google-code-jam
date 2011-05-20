261813ac1dbafaeee7fcbc34cb3e163521f3e27f
Yuval Adam <yuv.adm@gmail.com>
2011-05-20 11:18:40 +0300

    small changes to 2010 1c

--- 2010/1c/c/c.py
+++ 2010/1c/c/c.py
@@ -1,19 +1,16 @@
 f = open('1.in', 'r')
 o = open('1.out', 'w')
 
-M1 = 0xaaaaaaaa
-M0 = M1 >> 1
-
 T = int(f.readline().strip())
 
 for t in xrange(T):
     (M, N) = map(int, f.readline().strip().split(' '))
-    grid = [int(f.readline().strip(), 16) for _m in range(M)]
+    grid = [bin(int(f.readline().strip(), 16))[2:] for _m in range(M)]
     
     nextSquare = min(M, N)
     
     
-    res = grid
+    res = '\n'.join([x for x in grid])
     s = "Case #%d: %s\n" % (t+1, res)
     print s
     #o.write(s)
