be520100012b1a11f9021940e5472c7813e5d4c7
Yuval Adam <yuv.adm@gmail.com>
2011-05-21 04:35:32 +0300

    second attempt FAIL

--- 2011/1a/a/a.py
+++ 2011/1a/a/a.py
@@ -5,10 +5,6 @@ T = int(f.readline().strip())
 
 for t in xrange(T):
     (N, PD, PG) = map(int, f.readline().strip().split(' '))
-    
-    wt = N * PD / 100
-    wy = N * PG / 100
-
     res = 'Possible' if PD > PG else 'Broken'
     s = "Case #%d: %s\n" % (t+1, res)
     print s
